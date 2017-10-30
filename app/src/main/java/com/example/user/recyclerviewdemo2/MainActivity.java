ede7a4bcef55a6b4df08a0d89fa290153dd7ed0c
Artur <musayelyan099@gmail.com>
2017-10-31 02:07:28 +0400

    Initial commit

---
+++ app/src/main/java/com/example/user/recyclerviewdemo2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.recyclerviewdemo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//http://www.vogella.com/tutorials/AndroidRecyclerView/article.html#using-recyclerview
+//https://developer.android.com/training/material/lists-cards.html
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
