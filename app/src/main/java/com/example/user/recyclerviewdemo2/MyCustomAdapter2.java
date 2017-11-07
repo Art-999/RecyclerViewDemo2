d13fb41fbd738bb4e28a20f5f2e66041d68964f3
artur.musayelyan <musayelyan099@gmail.com>
2017-11-07 19:28:58 +0400

    03.11.2017 ArtCommit gorcic ( 2 )

--- app/src/main/java/com/example/user/recyclerviewdemo2/MyCustomAdapter2.java
+++ app/src/main/java/com/example/user/recyclerviewdemo2/MyCustomAdapter2.java
@@ -20,12 +20,20 @@ public class MyCustomAdapter2 extends RecyclerView.Adapter<MyCustomAdapter2.MyVi
     Context context;
     LayoutInflater inflater;
 
+
     public MyCustomAdapter2(Context context, List<Information> list) {
         this.context = context;
         this.dataList = list;
         //inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater = LayoutInflater.from(context);
+
     }
+
+    public void delete(int position){
+        dataList.remove(position);
+        notifyItemRemoved(position);
+    }
+
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -36,10 +44,18 @@ public class MyCustomAdapter2 extends RecyclerView.Adapter<MyCustomAdapter2.MyVi
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder,int position) {
         Information currentInformation = dataList.get(position);
         holder.titlesTextView.setText(currentInformation.title);
         holder.flagsImageView.setImageResource(currentInformation.iconId);
+
+//        //1-in tarberak
+//        holder.flagsImageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(context,"Item clicked at "+position,Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     @Override
@@ -47,7 +63,7 @@ public class MyCustomAdapter2 extends RecyclerView.Adapter<MyCustomAdapter2.MyVi
         return dataList.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView titlesTextView;
         ImageView flagsImageView;
 
@@ -55,8 +71,19 @@ public class MyCustomAdapter2 extends RecyclerView.Adapter<MyCustomAdapter2.MyVi
             super(itemView);
             titlesTextView = itemView.findViewById(R.id.textView_forCustomAdapter1);
             flagsImageView = itemView.findViewById(R.id.image_forCustomAdapter1);
+
+            itemView.setOnClickListener(this);
+        }
+
+
+        //2-rd tarberak
+        @Override
+        public void onClick(View v) {
+            //Toast.makeText(context,"Item clicked at "+getAdapterPosition(),Toast.LENGTH_SHORT).show();
+            delete(getAdapterPosition());
         }
     }
+
 
 
 }
